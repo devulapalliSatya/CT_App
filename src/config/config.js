26787b250498c7ffdc0cdae69e6df26d189d08fc
devulapalliSatya <devulapallisatya@outlook.com>
2023-08-23 15:55:23 +0530

    Project init

---
+++ src/config/config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    PORT: 4000,
+    DEV_PROJECT_KEY: "satya-devulapalli",
+    DEV_CLIENT_SECRET: "fGsy-HRSEf_izx_6D9tiWploMSg6FHHg",
+    DEV_CLIENT_ID: "Xk_C8zL2J6ptftAvrNa1su_T",
+    DEV_AUTH_URL: "https://auth.australia-southeast1.gcp.commercetools.com",
+    DEV_API_URL: "https://api.australia-southeast1.gcp.commercetools.com",
+    DEV_SCOPES: "manage_project:satya-devulapalli",
+    DEV_CUSTOMER_EMAIL: "satya@gmail.com",
+    DEV_CUSTOMER_PASSWORD: "satya@123"
+}
---
+++ src/resolvers/customer.js
@@ -0,0 +1,77 @@
+const { DEV_PROJECT_KEY, DEV_API_URL } = require("../config/config");
+const axios = require("axios");
+
+const commerceToolsApiUrl = `${DEV_API_URL}/${DEV_PROJECT_KEY}/graphql`;
+
+const { getAccessToken } = require("../middleware/token");
+
+async function getDataFromCommerceTool({ query, variables }) {
+    try {
+        const accessToken = await getAccessToken();
+        variables = variables || {};
+        const data = JSON.stringify({
+            query: query,
+            variables: Object.keys(variables).length ? variables : {}
+        });
+
+        const config = {
+            method: 'post',
+            url: commerceToolsApiUrl,
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': 'Bearer ' + accessToken
+            },
+            data
+        };
+        const resp = (await axios(config)).data;
+        return resp.data;
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+const resolvers = {
+    Query: {
+        async customers(_, __, contextValue) {
+            try {
+                const { query } = contextValue.req.body;
+                const data = await getDataFromCommerceTool({ query });
+                return data.customers;
+            } catch (error) {
+                console.log(error);
+            }
+        },
+
+        async customer(_, args, contextValue) {
+            try {
+                const { query, variables } = contextValue.req.body;
+                const data = await getDataFromCommerceTool({ query, variables });
+                return data.customer;
+            } catch (error) {
+                console.log(error);
+            }
+        },
+
+        async products(_, args, contextValue) {
+            try {
+                const { query } = contextValue.req.body;
+                const data = await getDataFromCommerceTool({ query });
+                return data.products;
+            } catch (error) {
+                console.log(error);
+            }
+        },
+
+        async product(_, args, contextValue) {
+            try {
+                const { query, variables } = contextValue.req.body;
+                const data = await getDataFromCommerceTool({ query, variables });
+                return data.product;
+            } catch (error) {
+                console.log(error);
+            }
+        },
+    }
+};
+
+module.exports = { resolvers };
