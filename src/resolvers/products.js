2ee7b00cf98e61c253a14e2ad1c6304930a47319
devulapalliSatya <devulapallisatya@outlook.com>
2023-08-24 11:52:58 +0530

    created service

--- src/resolvers/products.js
+++ src/resolvers/products.js
@@ -1,34 +1,8 @@
 const { DEV_PROJECT_KEY, DEV_API_URL } = require("../config/config");
 const axios = require("axios");
+const { getDataFromCommerceTool} = require('../service/resolver.service')
 
-const commerceToolsApiUrl = `${DEV_API_URL}/${DEV_PROJECT_KEY}/graphql`;
 
-const { getAccessToken } = require("../middleware/token");
-
-async function getDataFromCommerceTool({ query, variables }) {
-    try {
-        const accessToken = await getAccessToken();
-        variables = variables || {};
-        const data = JSON.stringify({
-            query: query,
-            variables: Object.keys(variables).length ? variables : {}
-        });
-
-        const config = {
-            method: 'post',
-            url: commerceToolsApiUrl,
-            headers: {
-                'Content-Type': 'application/json',
-                'Authorization': 'Bearer ' + accessToken
-            },
-            data
-        };
-        const resp = (await axios(config)).data;
-        return resp.data;
-    } catch (error) {
-        console.log(error)
-    }
-}
 
 const resolvers = {
     Query: {
---
+++ src/service/resolver.service.js
@@ -0,0 +1,31 @@
+const axios = require('axios')
+const commerceToolsApiUrl = `${DEV_API_URL}/${DEV_PROJECT_KEY}/graphql`;
+const { getAccessToken } = require("../middleware/token");
+
+
+module.exports = {
+    getDataFromCommerceTool: async ({ query, variables }) => {
+        try {
+            const accessToken = await getAccessToken();
+            variables = variables || {};
+            const data = JSON.stringify({
+                query: query,
+                variables: Object.keys(variables).length ? variables : {}
+            });
+
+            const config = {
+                method: 'post',
+                url: commerceToolsApiUrl,
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': 'Bearer ' + accessToken
+                },
+                data
+            };
+            const resp = (await axios(config)).data;
+            return resp.data;
+        } catch (error) {
+            console.log(error)
+        }
+    }
+}
